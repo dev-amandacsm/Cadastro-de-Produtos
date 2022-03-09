fafd3ca6113f84c4bba96bfbe9cfd656bb77b364
Gabriel Souza <gsouza590@gmail.com>
2022-03-09 10:07:59 -0300

    Tratamento de Excecoes personalizadas e configuracoes

--- src/main/java/com/projetweb/cadproduto/resources/excepetions/ResourceExceptionHandler.java
+++ src/main/java/com/projetweb/cadproduto/resources/excepetions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projetweb.cadproduto.services.exceptions.DatabaseException;
 import com.projetweb.cadproduto.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,16 @@ public class ResourceExceptionHandler {
 		
 		 String erro = "Recurso não encontrado";
 		 HttpStatus status = HttpStatus.NOT_FOUND;
+		 StandardError err = new StandardError(Instant.now(),status.value(),erro,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e , HttpServletRequest request){
+		
+		 String erro = "Erro no Database";
+		 HttpStatus status = HttpStatus.BAD_REQUEST;
 		 StandardError err = new StandardError(Instant.now(),status.value(),erro,e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		
--- src/main/java/com/projetweb/cadproduto/services/UsuarioService.java
+++ src/main/java/com/projetweb/cadproduto/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetweb.cadproduto.entities.Usuario;
 import com.projetweb.cadproduto.repositories.UsuarioRepository;
+import com.projetweb.cadproduto.services.exceptions.DatabaseException;
 import com.projetweb.cadproduto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UsuarioService {
 	}
 	
 	public void delete (Long id) {
+		try {
 		rep.deleteById(id);
+		}catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public Usuario update(Long id, Usuario obj) {
